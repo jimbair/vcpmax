81753145c13b65866d77b3754435975c85dc1dd2
tsuehpsyde <tsuehpsyde@storage.tsuehpsyde.com>
2010-01-22 12:22:54 -0600

    First commit.

--- vcpmax.py
+++ vcpmax.py
@@ -2,6 +2,22 @@
 # Quick script to help me with VMWare VCP Training.
 # Config built by hand from answers here:
 # http://www.vmware.com/pdf/vsphere4/r40/vsp_40_config_max.pdf
+#
+# Copyright (C) 2010  James Bair <james.d.bair@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 config = 'config.txt'
 raw = file(config, 'r')
